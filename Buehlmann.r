346d5448d62500f143ab83b3ac21dcca88932648
myunghoonju <myunghoonju@gmail.com>
2020-01-29 21:39:15 +0900

    Buehlman updated

--- Buehlmann.r
+++ Buehlmann.r
@@ -32,4 +32,30 @@ coef(glm2,which="") #select all vars
 plot(glm2,off2int=TRUE) 
 plot(glm2,ylim=range(coef(glm2,which=preds)))
 
-#fitting GAM:gamboost, centering?
+##Pred body fat
+gam1<-gamboost(DEXfat~bbs(hipcirc)+bbs(kneebreadth)
+    +bbs(anthro3a),data=bodyfat)
+
+
+par(mfrow=c(1,3))
+plot(gam1)
+
+#stopping is important to prevent overfitting
+#AIC-based stopping tends to overshoot the optimal point i.e.,mstop(cvm)
+#use cross-validated est of the empirical risk instead
+#convenience function cv() example
+cv(weights,type=c("bootstrap","kfold","subsampling"),
+    B=ifelse(type=="kfold",10,25))
+
+gam2<-gamboost(DEXfat~.,baselearner="bbs",data=bodyfat,
+    control=boost_control(trace=TRUE))
+cvm<-cvrisk(gam2) #25-fold bootstrap cross-val
+cvm
+plot(cvm)
+mstop(cvm)
+gam2[mstop(cvm)] #setting stopping point
+names(coef(gam2)) #selected base-learners at mstop(cvm)
+
+#To see that nothing got lost we now increase mstop to 1000:
+gam2[1000, return = FALSE] #iter 1-100 not computed
+names(coef(gam2))
